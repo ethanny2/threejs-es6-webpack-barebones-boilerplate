db457239327bd162bd1c641de911b659bf303bbd
Ethan <ethanny2@gmail.com>
2019-12-16 23:05:29 -0500

    Finally realized that it really didn't make sense to relegate different parts of the build process between Webpack and Gulp. Webpack is better at bundling JS files and works on every other type of file as well. Gulp doesn't dynamically bundle JS files based on dependencies, relies on a series of community supported plugins meaning more breaking etc... Im refactoring the build system to get rid of Gulp entirely. Right now I am just setting up an optimal webpack config.

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+
+import * as THREE from 'three';
+import * as TWEEN from '@tweenjs/tween.js';
+
+
+alert('testing the first successfull webpack build!');
--- webpack.config.js
+++ webpack.config.js
@@ -2,47 +2,63 @@ const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+
+/*Reference if current build is in dev or prod */
+const devMode = process.env.NODE_ENV !== 'production';
+
 module.exports = {
-	cache: true,
-	entry: './src/js/index.js',
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist'),
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader?cacheDirectory',
-					options: {
-						presets: ['@babel/preset-env'],
-					},
-				},
-			},
-			{
-				test: /\.(png|svg|jpg|gif)$/,
-				use: ['file-loader'],
-			},
-			{
-				test: /\.(ogg|mp3|wav|mpe?g)$/i,
-				use: ['file-loader'],
-			},
-		],
-	},
-	mode: 'development',
-	devtool: 'eval',
-	plugins: [
-		new HtmlWebpackPlugin({
-			title: 'PS2 NOISE SAMPLE',
-			filename: 'index.html',
-			template: './src/static/templates/index.html',
-			hash: true,
-		}),
-		// new webpack.DllReferencePlugin({
-		// 	context: __dirname,
-		// 	manifest: require('./library.json')
-		// })
-	],
+  cache: true,
+  entry: './src/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader?cacheDirectory',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(ogg|mp3|wav|mpe?g)$/i,
+        use: ['file-loader']
+      },
+      {
+		  //Targets all .scss, .sass , .css files (case-insensitive)
+        test: /\.s[ac]ss$/i,
+        use: [
+         // Translates CSS into CommonJS
+          'css-loader',
+          // Adds vendor prefixes with Autoprefixer
+          'postcss-loader',
+          // Compiles SASS to CSS
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  mode: 'development',
+  devtool: 'eval',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'PS2 NOISE SAMPLE',
+      filename: 'index.html',
+      template: './src/static/templates/index.html',
+      hash: true
+    })
+    // new webpack.DllReferencePlugin({
+    // 	context: __dirname,
+    // 	manifest: require('./library.json')
+    // })
+  ]
 };
